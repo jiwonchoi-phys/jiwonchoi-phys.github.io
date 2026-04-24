88c5daf9f948287abed4c18b5f8ccca71e4cb1e1
jiwonchoi-phys <chlwldnjs175@gmail.com>
2026-04-24 22:07:18 +0200

    init

---
+++ del_unnes.sh
@@ -0,0 +1,19 @@
+rm _posts/*
+rm _publications/*
+rm _pages/archive-layout-with-content.md
+rm _pages/markdown.md
+rm _pages/non-menu-page.md
+rm _pages/sitemap.md
+rm _pages/terms.md
+rm _pages/cv-json.md
+rm -r talkmap/
+rm _data/cv.json
+rm Dockerfile
+rm docker-compose.yaml
+rm _config_docker.yml
+rm images/500x300.png
+rm images/bio-photo.jpg
+rm images/bio-photo-2.jpg
+rm images/editing-talk.png
+rm images/themes/
+
